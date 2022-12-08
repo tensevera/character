a972167f2e53d5dcfb4cd8b6330108f045496852
tensevera <Jan-Severa@seznam.cz>
2022-12-08 18:42:50 +0100

    Fonts a tak

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+import time
 import pygame
 import random
 import math
@@ -11,6 +12,8 @@ screen = pygame.display.set_mode((display_size, display_size))
 character_size = 20
 character = pygame.Surface((character_size,character_size))
 character.fill((255,255,255))
+character_length = 1
+character_body = []
 #souradnice
 x = 0
 y = 0
@@ -24,15 +27,22 @@ step = 2
 snack = pygame.Surface((10,10))
 snack.fill((255,0,0))
 #souradnice pochutiny
-snack_x = random.randint(0,display_size)
+snack_x = random.randint(0,10)
 snack_y = random.randint(0,display_size)
 #promenne pochutiny
 snack_time = 5
+snack_spawn = time.time()
+#pochutina offset
+snack_offset = 20
 
 #pomocne promenne
 running = True
 points = 0
 
+#font
+font_size = 15
+score_font = pygame.font.SysFont("comicsansms", font_size)
+timer_font = pygame.font.SysFont("impact", font_size)
 
 clock = pygame.time.Clock()
 while running:
@@ -53,9 +63,6 @@ while running:
             elif event.key == pygame.K_w:
                 dy = -1
                 dx = 0
-        elif event.type == pygame.KEYUP:
-            dx = 0
-            dy = 0
     #posun
     x += step*dx
     y += step*dy
@@ -70,13 +77,33 @@ while running:
         y = 0
     #vykresli
     screen.fill((0,0,0))
-    screen.blit(character, (x,y))
+
     screen.blit(snack, (snack_x,snack_y))
     if (abs(snack_x-x) < character_size and abs(snack_y-y) < character_size):
-        snack_x = random.randint(0,display_size)
-        snack_y = random.randint(0,display_size)
+        snack_x = random.randint(snack_offset,display_size-snack_offset)
+        snack_y = random.randint(snack_offset,display_size-snack_offset)
+        snack_spawn = time.time()
+        character_length += 1
         points += 1
-        print(points)
+
+    if(time.time() - snack_spawn > snack_time):
+        points = 0
+        snack_x = random.randint(snack_offset,display_size-snack_offset)
+        snack_y = random.randint(snack_offset,display_size-snack_offset)
+        snack_spawn = time.time()
+    score_value = score_font.render("Your score: " + str(points), True, (255,255,255))
+    timer_value = timer_font.render("Timer: " + str(round(snack_time -time.time()+snack_spawn)), True, (255,255,255))
+    screen.blit(score_value, (0,0))
+    screen.blit(timer_value, (0,font_size))
+
+    snake_head = []
+    snake_head.append(x)
+    snake_head.append(y)
+
+    character_body.append(snake_head)
+    if (len(character_body) > character_length):
+        del character_body[0]
+
 
     pygame.display.update()
     #FPS
